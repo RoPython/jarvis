57c01a5fbd8d88a269a774e5839947da9b31b18a
Alexandru Coman <mail@alexcoman.com>
2014-06-08 16:21:09 +0300

    Some problems solved.

--- setup.py
+++ setup.py
@@ -16,11 +16,9 @@ setup(
     description=("Multi-purpose processing framework with frequently used"
                  " utilities"),
     long_description=open("README.md").read(),
-    author=("Cosmin Poieana", "Alexandru Coman"),
-    author_email=(
-        "cmin@ropython.org",
-        "alex@ropython.org"
-    ),
+    author=("Cosmin Poieana, Alexandru Coman"),
+    author_email=("Cosmin Poieana <cmin@ropython.org>, "
+                  "Alexandru Coman <alex@ropython.org>"),
     url="https://github.com/RoPython/jarvis",
     packages=["jarvis", "jarvis.utils", "jarvis.worker"],
     scripts=["scripts/jarvis"],
